f3f08918189c06e48a85cc74e292564e9d42ed20
kranthiAddanki <kranthi.addanki@my.jcu.edu.au>
2025-09-28 17:22:51 +1000

    Add score_menu.py program

---
+++ prac_02/score_menu.py
@@ -0,0 +1,39 @@
+def main():
+    MENU = "(G)et a valid score \n(P)rint result \n(S)how stars \n(Q)uit"
+    print(MENU)
+    choice = input("Enter your choice: ").upper()
+
+    while choice != "Q":
+        if choice == "G":
+            score = get_valid_score()
+        elif choice == "P":
+            print(print_result(score))
+        elif choice == "S":
+            print(print_astrisks(score))
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input("Enter your choice: ").upper()
+    print("Thanks")
+
+
+def get_valid_score():
+    score = int(input("Enter score: "))
+    return score
+
+
+def print_result(score):
+    if score < 0 or score > 100:
+        return "Enter a valid score"
+    elif score >= 90:
+        return "excellent"
+    elif score >= 50:
+        return "pass"
+    else:
+        return "bad"
+
+
+def print_astrisks(score):
+    return "*" * score
+
+main()
