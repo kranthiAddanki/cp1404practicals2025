d19805d042b00d32aec97f1deda83bb8e65f62f7
kranthiAddanki <kranthi.addanki@my.jcu.edu.au>
2025-09-30 17:21:08 +1000

    Complete capitalist_conrad.py program and randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,18 @@
+# print(random.randint(5, 20))
+# What was the smallest number you could have seen, what was the largest?
+#10 10 19 8 19
+# 12, 14, 19, 18
+
+
+# print(random.randrange(3, 10, 2))
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+#3 5 3 3 3
+# 9 9 3 5
+
+# print(random.uniform(2.5, 5.5))
+# What was the smallest number you could have seen, what was the largest?
+#2.6262749624230057, 4.0994946374012144, 2.787217133081052, 4.059073052858555, 4.7982111766965945
+#4.07011620787531, 5.41660719215922, 5.32040851132841, 3.39162970160789
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
