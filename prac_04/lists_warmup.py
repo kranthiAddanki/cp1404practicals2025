8629fbd4d5fa1fd39e5ef4789fbd17cd7f4528a7
kranthiAddanki <kranthi.addanki@my.jcu.edu.au>
2025-10-07 21:25:45 +1000

    Complete lists_warmup.py

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,22 @@
+
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# print(numbers[0])   #3
+# print(numbers[-1])  #2
+# print(numbers[3])   #1
+# print(numbers[:-1]) # [3, 1, 4, 1, 5, 9]
+# print(numbers[3:4]) # [1, 5]
+
+#1.
+numbers[0] = "ten"
+print(numbers)
+
+#2.
+numbers[-1] = 1
+print(numbers)
+
+#3.
+print(numbers[2:])
+
+#4.
+print(9 in numbers)
